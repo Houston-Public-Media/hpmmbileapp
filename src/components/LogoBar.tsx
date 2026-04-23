bf0030747d2d48da1095c9c79caa26c23f05aaf9
jwcounts <jared@jcounts.com>
2026-04-23 13:53:52 -0500

    Revised header with donate button

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import { Image } from 'react-native'
+import { Linking, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { DrawerNavigationProp } from '@react-navigation/drawer'
 import { fetchWeather } from '../services/newsApi';
@@ -9,7 +8,7 @@ import { Weather } from '../type';
 // Function to decode HTML entities
 import { cleanText } from '../utils/htmlUtils';
 
-const LogoBar: React.FC = () => {
+export const LogoBar: React.FC = () => {
   const navigation = useNavigation<DrawerNavigationProp<any>>();
   const [weather, setWeather] = useState<Weather>({
     icon: '',
@@ -44,6 +43,11 @@ const LogoBar: React.FC = () => {
       <TouchableOpacity onPress={handleLogoPress}>
         <Image source={{uri: `https://cdn.houstonpublicmedia.org/assets/images/houston-public-media-logo.png`}} style={styles.logo} />
       </TouchableOpacity>
+      <TouchableOpacity style={styles.donateButton}
+          onPress={() => Linking.openURL('https://www.houstonpublicmedia.org/donate')}
+        >
+          <Text style={styles.donateButtonText}>Donate</Text>
+        </TouchableOpacity>
       <View style={styles.textContainer}>
         <Text style={styles.dateText}>{currentDate}</Text>
         <View style={styles.weatherContainer}>
@@ -54,11 +58,7 @@ const LogoBar: React.FC = () => {
               alt={weather.description}
             />
           )}
-          <View style={styles.textContainer}>
-            <Text style={styles.tempText}>
-              {decodedTemperature}
-            </Text>
-          </View>
+          <Text style={styles.tempText}>{decodedTemperature}</Text>
         </View>
       </View>
     </View>
@@ -73,6 +73,7 @@ const styles = StyleSheet.create({
     padding: 10,
     height: 85,
     flexDirection: 'row',
+    flexWrap: 'wrap'
   },
   logo: {
     width: 270,
@@ -80,8 +81,11 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   textContainer: {
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
     alignItems: 'center',
+    width: '100%',
+    paddingBlockStart: 5
   },
   dateText: {
     color: '#fff',
@@ -103,6 +107,14 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
   },
+  donateButton: {
+    padding: 10,
+    borderRadius: 14,
+    backgroundColor: '#C8102E'
+  },
+  donateButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
 })
-
-export default LogoBar
