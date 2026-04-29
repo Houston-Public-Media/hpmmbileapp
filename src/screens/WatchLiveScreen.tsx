6843f98d8f763706b06bba1d17d541940b5749d9
sapate34 <sapate34@central.uh.edu>
2026-04-29 15:46:01 -0500

    Solved video streaming issue
    Solved video streaming issue

--- src/screens/WatchLiveScreen.tsx
+++ src/screens/WatchLiveScreen.tsx
@@ -7,12 +7,16 @@ import TalkshowBanner from '../components/TalkshowBanner';
 import AudioFooter from '../components/AudioFooter';
 import { fetchPriorityData } from "../services/newsApi";
 import { TalkshowEntry } from '../type';
+/*
+ ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1'
+    : 'Smitas Phone, Android phone, Iphone, Safari, mobile app, Chrome';Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36
 
+*/
 const WatchLiveScreen = () => {
 	const userAgent =
 		Platform.OS === 'ios'
 			? 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1'
-			: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36';
+			: 'Smitas Phone, Android phone, Iphone, Safari, mobile app, Chrome';
 
 	const [talkshowData, setTalkshowData] = useState<TalkshowEntry[]>([]);
 	const [breakingData, setBreakingData] = useState<any>(null);
@@ -59,7 +63,7 @@ const WatchLiveScreen = () => {
 						javaScriptEnabled={true}
 						domStorageEnabled={true}
 						allowsInlineMediaPlayback={true}
-						mediaPlaybackRequiresUserAction={false}
+						mediaPlaybackRequiresUserAction={true}
 						allowsFullscreenVideo={true}
 						automaticallyAdjustContentInsets={false}
 						mixedContentMode="always"
